748a55d5851722aa42572e7438abcdd43f92ce05
ekko <fqsy1416@gmail.com>
2025-11-08 08:30:51 +0800

    更换rpc

--- frontend/src/stores/sdkStore.ts
+++ frontend/src/stores/sdkStore.ts
@@ -42,7 +42,28 @@ export const useSDKStore = defineStore('sdk', () => {
       } else {
         await initSDK()
       }
-      const instance = await createInstance(SepoliaConfig);
+      const instance = await createInstance({
+        // ACL_CONTRACT_ADDRESS (FHEVM Host chain)
+        aclContractAddress: '0x687820221192C5B662b25367F70076A37bc79b6c',
+        // KMS_VERIFIER_CONTRACT_ADDRESS (FHEVM Host chain)
+        kmsContractAddress: '0x1364cBBf2cDF5032C47d8226a6f6FBD2AFCDacAC',
+        // INPUT_VERIFIER_CONTRACT_ADDRESS (FHEVM Host chain)
+        inputVerifierContractAddress: '0xbc91f3daD1A5F19F8390c400196e58073B6a0BC4',
+        // DECRYPTION_ADDRESS (Gateway chain)
+        verifyingContractAddressDecryption:
+          '0xb6E160B1ff80D67Bfe90A85eE06Ce0A2613607D1',
+        // INPUT_VERIFICATION_ADDRESS (Gateway chain)
+        verifyingContractAddressInputVerification:
+          '0x7048C39f048125eDa9d678AEbaDfB22F7900a29F',
+        // FHEVM Host chain id
+        chainId: 11155111,
+        // Gateway chain id
+        gatewayChainId: 55815,
+        // Optional RPC provider to host chain
+        network: 'https://1rpc.io/sepolia',
+        // Relayer URL
+        relayerUrl: 'https://relayer.testnet.zama.cloud',
+      });
       // 2. 创建 SDK 实例（使用默认配置）
       const sdk = new PrivateTransferSDK()
       // 3. 初始化 SDK（使用钱包 provider 或不使用）
